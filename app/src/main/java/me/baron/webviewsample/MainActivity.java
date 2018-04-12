fda556031cbb09f4793799f5a45e7830e4d8cb33
thisfeng <23006239@qq.com>
2018-04-12 11:09:16 +0800

    解决再次点击上传图片时无法响应问题

--- app/src/main/java/me/baron/webviewsample/MainActivity.java
+++ app/src/main/java/me/baron/webviewsample/MainActivity.java
@@ -1,47 +1,45 @@
 package me.baron.webviewsample;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.ClipData;
-import android.content.ComponentName;
-import android.content.Context;
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.net.Uri;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Parcelable;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
- * 使用前务必允许文件和拍照权限,以下省略此步 自行根据项目获取权限或直接去设置页面手动打开 ，否则无法打开相机
  *
+ * 
  * fix 7.0文件访问，测试OK 8.0； 7.0； 6.0 ；4.4
  */
 public class MainActivity extends AppCompatActivity {
 
     private ValueCallback<Uri> uploadMessage;
     private ValueCallback<Uri[]> uploadMessageAboveL;
-    private final static int FILE_CHOOSER_RESULT_CODE = 10000;
 
-    /**
-     * 拍照的临时图片uri
-     */
-    private Uri tokeImageUri;
+    private static final int REQUEST_CODE_ALBUM = 0x01;
+    private static final int REQUEST_CODE_CAMERA = 0x02;
+    private static final int REQUEST_CODE_PERMISSION_CAMERA = 0x03;
+
+    private String mCurrentPhotoPath;
+    private String mLastPhotoPath;
 
 
     @Override
@@ -60,24 +58,21 @@ public class MainActivity extends AppCompatActivity {
             // For Android < 3.0
             public void openFileChooser(ValueCallback<Uri> valueCallback) {
                 uploadMessage = valueCallback;
-//                openImageChooserActivity();
-                takePhotoOrOpenFileChooser();
-
+                uploadPicture();
             }
 
             // For Android  >= 3.0
             public void openFileChooser(ValueCallback valueCallback, String acceptType) {
                 uploadMessage = valueCallback;
-//                openImageChooserActivity();
-                takePhotoOrOpenFileChooser();
+                uploadPicture();
+
 
             }
 
             //For Android  >= 4.1
             public void openFileChooser(ValueCallback<Uri> valueCallback, String acceptType, String capture) {
                 uploadMessage = valueCallback;
-//                openImageChooserActivity();
-                takePhotoOrOpenFileChooser();
+                uploadPicture();
 
             }
 
@@ -85,8 +80,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
                 uploadMessageAboveL = filePathCallback;
-//                    openImageChooserActivity();
-                takePhotoOrOpenFileChooser();
+                uploadPicture();
                 return true;
             }
         });
@@ -94,123 +88,193 @@ public class MainActivity extends AppCompatActivity {
         webview.loadUrl(targetUrl);
     }
 
-    private void openImageChooserActivity() {
-        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-        i.addCategory(Intent.CATEGORY_OPENABLE);
-        i.setType("image/*");
-        startActivityForResult(Intent.createChooser(i, "Image Chooser"), FILE_CHOOSER_RESULT_CODE);
-    }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == FILE_CHOOSER_RESULT_CODE) {
-            if (null == uploadMessage && null == uploadMessageAboveL) {
+
+        if (requestCode == REQUEST_CODE_ALBUM || requestCode == REQUEST_CODE_CAMERA) {
+            if (uploadMessage == null && uploadMessageAboveL == null) {
                 return;
             }
-            Uri result = data == null || resultCode != RESULT_OK ? null : data.getData();
-            if (uploadMessageAboveL != null) {
 
-                onActivityResultAboveL(requestCode, resultCode, data);
-            } else if (uploadMessage != null) {
-                if (result != null) {
-                    uploadMessage.onReceiveValue(result);
-                } else {
-                    //result等于空的话是拍照的
-                    uploadMessage.onReceiveValue(tokeImageUri);
+            //取消拍照或者图片选择时
+            if (resultCode != RESULT_OK) {
+                //一定要返回null,否则<input file> 就是没有反应
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+                if (uploadMessageAboveL != null) {
+                    uploadMessageAboveL.onReceiveValue(null);
+                    uploadMessageAboveL = null;
+
                 }
             }
-            uploadMessage = null;
 
+            //拍照成功和选取照片时
+            if (resultCode == RESULT_OK) {
+                Uri imageUri = null;
+
+                switch (requestCode) {
+                    case REQUEST_CODE_ALBUM:
+
+                        if (data != null) {
+                            imageUri = data.getData();
+                        }
+
+                        break;
+                    case REQUEST_CODE_CAMERA:
+
+                        if (!TextUtils.isEmpty(mCurrentPhotoPath)) {
+                            File file = new File(mCurrentPhotoPath);
+                            Uri localUri = Uri.fromFile(file);
+                            Intent localIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, localUri);
+                            sendBroadcast(localIntent);
+                            imageUri = Uri.fromFile(file);
+                            mLastPhotoPath = mCurrentPhotoPath;
+                        }
+                        break;
+                    default:
+                }
+
+
+                //上传文件
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(imageUri);
+                    uploadMessage = null;
+                }
+                if (uploadMessageAboveL != null) {
+                    uploadMessageAboveL.onReceiveValue(new Uri[]{imageUri});
+                    uploadMessageAboveL = null;
+
+                }
+            }
         }
     }
 
+
     /**
-     * 5.0以上
+     * 选择相机或者相册
      */
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void onActivityResultAboveL(int requestCode, int resultCode, Intent intent) {
-        if (requestCode != FILE_CHOOSER_RESULT_CODE || uploadMessageAboveL == null) {
-            return;
-        }
-        Uri[] results = null;
-        if (resultCode == Activity.RESULT_OK) {
-            if (intent != null) {
-                String dataString = intent.getDataString();
-                ClipData clipData = intent.getClipData();
-                if (clipData != null) {
-                    results = new Uri[clipData.getItemCount()];
-                    for (int i = 0; i < clipData.getItemCount(); i++) {
-                        ClipData.Item item = clipData.getItemAt(i);
-                        results[i] = item.getUri();
-                    }
+    public void uploadPicture() {
+        String[] items = new String[]{"相机", "相册"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("请选择图片上传方式");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        if (!TextUtils.isEmpty(mLastPhotoPath)) {
+                            File file = new File(mLastPhotoPath);
+                            if (file != null) {
+                                file.delete();
+                            }
+                        }
+                        //请求拍照权限
+                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                            takePhoto();
+                        } else {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_PERMISSION_CAMERA);
+                        }
+
+                        break;
+                    case 1:
+                        chooseAlbumPic();
+                        break;
+                    default:
                 }
-                if (dataString != null) {
-                    results = new Uri[]{Uri.parse(dataString)};
-                    uploadMessageAboveL.onReceiveValue(results);
-                } else {
-                    results = new Uri[]{tokeImageUri};
-                    uploadMessageAboveL.onReceiveValue(results);
-                }
-            } else {
-                results = new Uri[]{tokeImageUri};
-                uploadMessageAboveL.onReceiveValue(results);
             }
-        }
-        uploadMessageAboveL = null;
+        });
+
+        //取消对话框时候置空
+        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+
+                //一定要返回null,否则<input type='file'>
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+                if (uploadMessageAboveL != null) {
+                    uploadMessageAboveL.onReceiveValue(null);
+                    uploadMessageAboveL = null;
+
+                }
+            }
+        });
+
+
+        builder.show();
+
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_CODE_PERMISSION_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePhoto();
+                } else {
+                    // Permission Denied
+                    new AlertDialog.Builder(this)
+                            .setTitle("无法拍照")
+                            .setMessage("您未授予拍照权限")
+                            .setNegativeButton("取消", null)
+                            .setPositiveButton("去设置", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent localIntent = new Intent();
+                                    localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    localIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+                                    localIntent.setData(Uri.fromParts("package", getPackageName(), null));
+                                    startActivity(localIntent);
+                                }
+                            }).show();
+                }
+                break;
+            default:
+        }
+
+    }
 
     /**
-     * 系统方式 打开拍照或文件选择
+     * 拍照
      */
-    private void takePhotoOrOpenFileChooser() {
+    private void takePhoto() {
 
-        File file = new File(Environment.getExternalStorageDirectory(), "/temp/" + System.currentTimeMillis() + ".jpg");
-        if (!file.getParentFile().exists()) {
-            file.getParentFile().mkdirs();
-        }
-        //通过FileProvider创建一个content:// 前缀类型的Uri
-        tokeImageUri = getUriForFile(this, file);
-        final List<Intent> cameraIntents = new ArrayList<>();
-        final Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        final PackageManager packageManager = getPackageManager();
-        final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
-        for (ResolveInfo res : listCam) {
-            final String packageName = res.activityInfo.packageName;
-            final Intent i = new Intent(captureIntent);
-            i.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
-            i.setPackage(packageName);
-            i.putExtra(MediaStore.EXTRA_OUTPUT, tokeImageUri);
-            cameraIntents.add(i);
+        StringBuilder fileName = new StringBuilder();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri takeUri = null;
+
+        fileName.append(System.currentTimeMillis()).append("_temp.png");
+        File tempFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), fileName.toString());
+        //适配7.0文件访问,通过此方式拿到文件地址
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            takeUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".android7.fileprovider", tempFile);
+        } else {
+            takeUri = Uri.fromFile(tempFile);
         }
 
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, takeUri);
 
+        mCurrentPhotoPath = tempFile.getAbsolutePath();
+        startActivityForResult(intent, REQUEST_CODE_CAMERA);
+
+    }
+
+    /**
+     * 选择相册照片
+     */
+    private void chooseAlbumPic() {
         Intent i = new Intent(Intent.ACTION_GET_CONTENT);
         i.addCategory(Intent.CATEGORY_OPENABLE);
         i.setType("image/*");
-        Intent chooserIntent = Intent.createChooser(i, "图像选择器");
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new Parcelable[]{}));
-        startActivityForResult(chooserIntent, FILE_CHOOSER_RESULT_CODE);
-    }
-
-    /**
-     * 适配7.0文件访问,通过此方式拿到文件地址
-     * Uri fileUri = FileProvider7.getUriForFile(this, file);
-     *
-     * @param context
-     * @param file
-     * @return
-     */
-
-    public static Uri getUriForFile(Context context, File file) {
-        Uri fileUri = null;
-        if (Build.VERSION.SDK_INT >= 24) {
-            fileUri = FileProvider.getUriForFile(context, context.getPackageName() + ".android7.fileprovider", file);
-        } else {
-            fileUri = Uri.fromFile(file);
-        }
-        return fileUri;
+        startActivityForResult(Intent.createChooser(i, "Image Chooser"), REQUEST_CODE_ALBUM);
     }
 }
